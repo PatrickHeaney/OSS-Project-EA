ff3c5aa3ce9fae1a1c57dc64ba56ef538bb1df64
Patrick Heaney <patrick.heaney@gmail.com>
2024-12-27 17:29:08 -0500

    Update from plantuml import PlantUML.py
    changed uml_code = line to point to the osspa.plantuml file

--- from plantuml import PlantUML.py
+++ from plantuml import PlantUML.py
@@ -4,32 +4,9 @@ import os
 # Define the PlantUML server URL
 plantuml_server = PlantUML(url='http://www.plantuml.com/plantuml/svg/')
 
-# Define the PlantUML diagram
-uml_code = """
-@startuml
-title Open Source Software (OSS) Project Architecture
-
-package "Frontend" {
-    [Web Application]
-}
-
-package "Backend" {
-    [API Server]
-    [Database]
-}
-
-package "CI/CD" {
-    [Jenkins]
-    [GitHub Actions]
-}
-
-[Web Application] --> [API Server]
-[API Server] --> [Database]
-[GitHub Actions] --> [API Server]
-[Jenkins] --> [API Server]
-
-@enduml
-"""
+# Read the PlantUML diagram from the file
+with open('osspa.plantuml', 'r') as file:
+    uml_code = file.read()
 
 # Generate the diagram and save it to a file
 output_file = 'diagram.svg'
